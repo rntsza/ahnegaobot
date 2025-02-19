7a178b5bbca200c19145b441bc1ec4c971b0c9c4
Renato Souza <reegomes@outlook.com.br>
2025-02-19 19:11:16 -0300

    Remove base-64 dependency and streamline server initialization in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 require("dotenv").config();
 require("../instrument.js");
 const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 10000;
+console.log("Iniciando servidor web...");
+app.listen(PORT, () => console.log(`Servidor web iniciado na porta ${process.env.PORT}`));
+console.log("Iniciado servidor web...");
+
 const client = require("./config/discordClient");
 const prisma = require("./config/prismaClient");
 const CommandService = require("./services/commandService");
@@ -8,12 +14,9 @@ const Sentry = require("@sentry/node");
 const cron = require("node-cron");
 const memeMonitorService = require("./services/memeMonitorService");
 const { messageHandler } = require("./services/messageHandlerService");
-const PORT = process.env.PORT || 10000;
-console.log("1");
-const app = express();
+
 const commandService = new CommandService(client);
-//const ready = require("./events/ready");
-console.log("2");
+
 client.once("ready", async () => {
   console.log(`Logado como ${client.user.tag}`);
   await commandService.registerSlashCommands();
@@ -21,18 +24,18 @@ client.once("ready", async () => {
     console.log("Executando memeMonitorService...");
     await memeMonitorService(client);
   });
-  //await ready(client);
 });
-console.log("3");
+
+
 client.on("interactionCreate", (interaction) => commandService.executeCommand(interaction));
 client.on("messageCreate", messageHandler);
-console.log("4");
+
 client.login(process.env.DISCORD_TOKEN);
-console.log("5");
+
 app.get("/", (req, res) => res.send("Bot do Discord está ativo e funcionando!"));
 app.get("/status", async (req, res) => res.json({ status: "Bot está ativo", totalPosts: await prisma.postedMeme.count() }));
 app.get("/posted-memes", async (req, res) => res.json(await prisma.postedMeme.findMany()));
-console.log("6");
+
 app.get("/check-memes-now", async (req, res) => {
   try {
     await memeMonitorService(client);
@@ -43,5 +46,3 @@ app.get("/check-memes-now", async (req, res) => {
     res.status(500).json({ error: "Erro ao acionar o monitoramento de memes." });
   }
 });
-console.log("7");
-app.listen(PORT || 10000, () => console.log(`Servidor web iniciado na porta ${process.env.PORT || 10000}`));
